bfc7ab10d94f776938bd54d1c4e73b067df55376
root <root@localhost.localdomain>
2011-07-13 22:30:47 -0700

    stuff

--- modules/custom/lib/puppet/provider/rcsrepo/git.rb
+++ modules/custom/lib/puppet/provider/rcsrepo/git.rb
@@ -2,4 +2,25 @@ Puppet::Type.type(:rcsrepo).provide(:git) do
   desc "This is the git provider for the rcsrepo type"
 
   commands :git => 'git'
+
+  def exists?
+    File.directory?(resource[:path] + '/.git')
+  end
+
+  def create
+    # If the source property is set, `git clone` should be used to pull it down to path
+    # Otherwise, an empty repository should be created with `git init`
+
+    if resource[:source]
+      FileUtils.rm_rf resource[:path] if File.exists? resource[:path]
+      git("clone", resource[:source], resource[:path])
+    else
+      FileUtils.mkdir_p resource[:path] if ! File.directory? resource[:path]
+      git_init resource[:path]
+    end
+  end
+
+  def git_init(path)
+    git("--git-dir", path + '/.git', "--work-tree", path, "init")
+  end
 end
--- modules/custom/spec/unit/provider/rcsrepo/git_spec.rb
+++ modules/custom/spec/unit/provider/rcsrepo/git_spec.rb
@@ -1,5 +1,10 @@
 require 'puppet'
 require 'fileutils'
+require 'mocha'
+RSpec.configure do |config|
+    config.mock_with :mocha
+end
+
 provider_class = Puppet::Type.type(:rcsrepo).provider(:git)
 describe provider_class do
   before :each do
@@ -8,10 +13,33 @@ describe provider_class do
     @provider = provider_class.new(@resource)
   end
 
+  it 'should ensure that a git directory does not exist' do
+    @provider.exists?.should == false
+  end
+
+  it 'should create a directory for a new git init' do
+    FileUtils.expects(:mkdir_p)
+    FileUtils.expects(:rm_rf).never
+    @provider.expects(:git).with("--git-dir", @provider.resource[:path] + '/.git', "--work-tree", @provider.resource[:path], "init")
+
+    @provider.create
+  end
+
+  it 'should not create a directory for a git clone' do
+    @provider.resource[:source] = 'https://github.com/jordansissel/puppet-examples.git'
+    FileUtils.expects(:mkdir_p).never
+    FileUtils.expects(:rm_rf).never #the dir doesn't exist
+    @provider.expects(:git).with("clone", @provider.resource[:source], @provider.resource[:path])
+
+    @provider.create
+  end
+
   it 'should ensure that a git directory exists' do
-    @provider
+    @provider.create
+    @provider.exists?.should == true
+  end
 
   after :each do
-    FileUtils.rm_rf(@test_dir)
+    FileUtils.rm_rf(@test_dir) if File.exists?(@test_dir)
   end
 end
